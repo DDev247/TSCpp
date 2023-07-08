f1c1d61f54c9b86175909c4b9812956c7d9a9526
Vrobocio <87225187+DDev247@users.noreply.github.com>
2023-07-08 16:51:59 +0200

    Revert to CRCpp

--- TSCpp/src/page_crc.cpp
+++ TSCpp/src/page_crc.cpp
@@ -4,47 +4,20 @@
 #include "../include/table3d_axis_io.h"
 #include <assert.h>
 
-uint32_t page_crctbl[256];
-
-void page_gentbl(void)
-{
-    uint32_t crc;
-    uint32_t c;
-    uint32_t i;
-    for (c = 0; c < 0x100; c++) {
-        crc = c;
-        for (i = 0; i < 8; i++) {
-            crc = (crc & 1) ? (crc >> 1) ^ 0xedb88320 : (crc >> 1);
-        }
-        page_crctbl[c] = crc;
-    }
-}
-
-uint32_t page_crc32(uint8_t* bfr, size_t size)
-{
-    uint32_t crc = 0xfffffffful;
-    while (size--)
-        crc = (crc >> 8) ^ page_crctbl[(crc & 0xff) ^ *bfr++];
-    return(crc ^ 0xfffffffful);
-}
-
-static inline uint32_t compute_raw_crc(const page_iterator_t &entity, bool update, uint32_t crc)
+static inline uint32_t compute_raw_crc(const page_iterator_t& entity, bool update, uint32_t crc)
 {
     if (update)
-        return page_crc32((uint8_t*)entity.pData, entity.size);
-    //return CRC::Calculate((uint8_t*)entity.pData, entity.size, CRC::CRC_32(), crc);
-    
-    page_gentbl();
-    return page_crc32((uint8_t*)entity.pData, entity.size);
+        return CRC::Calculate((uint8_t*)entity.pData, entity.size, CRC::CRC_32(), crc);
+
+    return CRC::Calculate((uint8_t*)entity.pData, entity.size, CRC::CRC_32());
 }
 
-static inline uint32_t compute_row_crc(const table_row_iterator &row, bool update, uint32_t crc)
+static inline uint32_t compute_row_crc(const table_row_iterator& row, bool update, uint32_t crc)
 {
-    if(update)
-        return page_crc32((uint8_t*)*row, row.size());
-    
-    page_gentbl();
-    return page_crc32((uint8_t*)*row, row.size());
+    if (update)
+        return CRC::Calculate(&*row, row.size(), CRC::CRC_32(), crc);
+
+    return CRC::Calculate(&*row, row.size(), CRC::CRC_32());
 }
 
 static inline uint32_t compute_tablevalues_crc(table_value_iterator it)
@@ -62,39 +35,37 @@ static inline uint32_t compute_tablevalues_crc(table_value_iterator it)
 
 static inline uint32_t compute_tableaxis_crc(table_axis_iterator it, uint32_t crc)
 {
-    const int16_byte *pConverter = table3d_axis_io::get_converter(it.get_domain());
-
+    const int16_byte* pConverter = table3d_axis_io::get_converter(it.get_domain());
     byte values[32]; // Fingers crossed we don't have a table bigger than 32x32
-    byte *pValue = values;
+    byte* pValue = values;
     while (!it.at_end())
     {
         *pValue++ = pConverter->to_byte(*it);
         ++it;
     }
-    return pValue-values==0 ? crc : page_crc32(values, pValue-values);
+    return pValue - values == 0 ? crc : CRC::Calculate(values, pValue - values, CRC::CRC_32(), crc);
 }
 
-static inline uint32_t compute_table_crc(const page_iterator_t &entity, bool update)
+static inline uint32_t compute_table_crc(const page_iterator_t& entity, bool update)
 {
-    return compute_tableaxis_crc(y_begin(entity), 
-                compute_tableaxis_crc(x_begin(entity),
-                    compute_tablevalues_crc(rows_begin(entity))
-                ));
+    return compute_tableaxis_crc(y_begin(entity),
+        compute_tableaxis_crc(x_begin(entity),
+            compute_tablevalues_crc(rows_begin(entity))
+        ));
 }
 
 static inline uint32_t pad_crc(uint16_t padding, uint32_t crc)
 {
-    uint8_t raw_value = 0u;
-    while (padding>0)
+    const uint8_t raw_value = 0u;
+    while (padding > 0)
     {
-        //crc = CRC::Calculate(&raw_value, 1, CRC::CRC_32(), crc);
-        crc = page_crc32(&raw_value, 1);
+        crc = CRC::Calculate(&raw_value, 1, CRC::CRC_32(), crc);
         --padding;
     }
     return crc;
 }
 
-static inline uint32_t compute_crc(const page_iterator_t &entity, bool update, uint32_t crc)
+static inline uint32_t compute_crc(const page_iterator_t& entity, bool update, uint32_t crc)
 {
     switch (entity.type)
     {
@@ -121,9 +92,9 @@ uint32_t calculatePageCRC32(byte pageNum)
     page_iterator_t entity = page_begin(pageNum);
     // Initial CRC calc
     uint32_t crc = compute_crc(entity, false, 0);
-
     entity = advance(entity);
-    while (entity.type!=End)
+    
+    while (entity.type != End)
     {
         crc = compute_crc(entity, true, crc);
         entity = advance(entity);
