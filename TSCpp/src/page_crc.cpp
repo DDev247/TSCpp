101ba8972702d5033cf11860f6799e60e27ffb25
Vrobocio <87225187+DDev247@users.noreply.github.com>
2023-07-08 14:57:02 +0200

    Page CRC fix?

--- TSCpp/src/page_crc.cpp
+++ TSCpp/src/page_crc.cpp
@@ -4,20 +4,47 @@
 #include "../include/table3d_axis_io.h"
 #include <assert.h>
 
+uint32_t page_crctbl[256];
+
+void page_gentbl(void)
+{
+    uint32_t crc;
+    uint32_t c;
+    uint32_t i;
+    for (c = 0; c < 0x100; c++) {
+        crc = c;
+        for (i = 0; i < 8; i++) {
+            crc = (crc & 1) ? (crc >> 1) ^ 0xedb88320 : (crc >> 1);
+        }
+        page_crctbl[c] = crc;
+    }
+}
+
+uint32_t page_crc32(uint8_t* bfr, size_t size)
+{
+    uint32_t crc = 0xfffffffful;
+    while (size--)
+        crc = (crc >> 8) ^ page_crctbl[(crc & 0xff) ^ *bfr++];
+    return(crc ^ 0xfffffffful);
+}
+
 static inline uint32_t compute_raw_crc(const page_iterator_t &entity, bool update, uint32_t crc)
 {
-    if(update)
-        return CRC::Calculate((uint8_t*)entity.pData, entity.size, CRC::CRC_32(), crc);
+    if (update)
+        return page_crc32((uint8_t*)entity.pData, entity.size);
+    //return CRC::Calculate((uint8_t*)entity.pData, entity.size, CRC::CRC_32(), crc);
     
-    return CRC::Calculate((uint8_t*)entity.pData, entity.size, CRC::CRC_32());
+    page_gentbl();
+    return page_crc32((uint8_t*)entity.pData, entity.size);
 }
 
 static inline uint32_t compute_row_crc(const table_row_iterator &row, bool update, uint32_t crc)
 {
     if(update)
-        return CRC::Calculate(&*row, row.size(), CRC::CRC_32(), crc);
+        return page_crc32((uint8_t*)*row, row.size());
     
-    return CRC::Calculate(&*row, row.size(), CRC::CRC_32());
+    page_gentbl();
+    return page_crc32((uint8_t*)*row, row.size());
 }
 
 static inline uint32_t compute_tablevalues_crc(table_value_iterator it)
@@ -44,7 +71,7 @@ static inline uint32_t compute_tableaxis_crc(table_axis_iterator it, uint32_t cr
         *pValue++ = pConverter->to_byte(*it);
         ++it;
     }
-    return pValue-values==0 ? crc : CRC::Calculate(values, pValue-values, CRC::CRC_32(), crc);
+    return pValue-values==0 ? crc : page_crc32(values, pValue-values);
 }
 
 static inline uint32_t compute_table_crc(const page_iterator_t &entity, bool update)
@@ -57,10 +84,11 @@ static inline uint32_t compute_table_crc(const page_iterator_t &entity, bool upd
 
 static inline uint32_t pad_crc(uint16_t padding, uint32_t crc)
 {
-    const uint8_t raw_value = 0u;
+    uint8_t raw_value = 0u;
     while (padding>0)
     {
-        crc = CRC::Calculate(&raw_value, 1, CRC::CRC_32(), crc);
+        //crc = CRC::Calculate(&raw_value, 1, CRC::CRC_32(), crc);
+        crc = page_crc32(&raw_value, 1);
         --padding;
     }
     return crc;
